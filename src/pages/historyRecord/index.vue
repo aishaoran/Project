fe44c9a1cfe33c9a05ff578417d3a5cb93681f0a
小宋 <shixi_shaoran@staff.sina.com.cn>
2019-03-25 17:56:16 +0800

    sd

--- src/pages/historyRecord/index.vue
+++ src/pages/historyRecord/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="historyIndex">
+    <now></now>
     历史记录的公共页，历史记录的所有子页面都有此段
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import now from '@/components/now/index'
   export default {
     name: 'historyIndex',
+    components:{now},
     data() {
       return {
       }
